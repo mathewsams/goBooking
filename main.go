cafb4cd2849348894f89cd7bfb393cfb2db6f379
mathewsams <sammathew@outlook.com>
2022-11-26 15:37:05 +0400

    Variables and Dataa Types

--- main.go
+++ main.go
@@ -5,6 +5,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("Sam Mathew")
+	const eventTickets int = 50
+	// var eventName string = "Go Event 22"
+	// Alternate Syntax to declare variables - Syntatic Sugar :)
+	eventName := "Go Event 22"
+	var remainingTickets uint = 50
+
+	// fmt.Println(eventName)
+
+	fmt.Println("Welcome to the", eventName, "application")
+	fmt.Printf("Total tickets %v\n", eventTickets)
+	fmt.Println("Get your tickets here to attend the event")
+
+	// Print Formating
+	fmt.Printf("Total tickets Remaining are %v\n", remainingTickets)
+
+	var userName string
+	var userTickets int
+
+	userName = "Sam"
+	userTickets = 20
+
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Printf("UserName type: %T and type of userTickets: %T\n", userName, userTickets)
+
 }
