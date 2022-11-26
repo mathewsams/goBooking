a49a4d72de2076f4ce755658dfdf4a2d17e243af
mathewsams <sammathew@outlook.com>
2022-11-26 16:07:12 +0400

    Getting user input from CLI

--- main.go
+++ main.go
@@ -11,22 +11,35 @@ func main() {
 	eventName := "Go Event 22"
 	var remainingTickets uint = 50
 
+	// fmt.Println("Memory Location of the variable eventName is:", &eventName)
 	// fmt.Println(eventName)
 
 	fmt.Println("Welcome to the", eventName, "application")
+	// Print Formating
 	fmt.Printf("Total tickets %v\n", eventTickets)
 	fmt.Println("Get your tickets here to attend the event")
 
-	// Print Formating
-	fmt.Printf("Total tickets Remaining are %v\n", remainingTickets)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-	var userName string
-	var userTickets int
+	fmt.Println("Enter you first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Sam"
-	userTickets = 20
+	fmt.Println("Enter you lastname: ")
+	fmt.Scan(&lastName)
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
-	fmt.Printf("UserName type: %T and type of userTickets: %T\n", userName, userTickets)
+	fmt.Println("Enter you email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets required: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\nYou will receive booking details in you registered email %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("\nTotal tickets remaining for the event %v\n", remainingTickets)
+	// fmt.Printf("firstName type: %T and type of userTickets: %T\n", firstName, userTickets)
 
 }
